ad11494ec33d49719745fd9cea61d7e1869c6efa
yzd <404013419@qq.com>
2019-04-21 20:05:05 +0800

    剑指offer题解

---
+++ jianzhiOffer/Solution_64.java
@@ -0,0 +1,41 @@
+package jianzhiOffer;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+
+public class Solution_64 {
+    public ArrayList<Integer> maxInWindows(int [] num, int size)
+    {
+        ArrayList<Integer> result = new ArrayList<>();
+        // 排除特殊情况，窗口的长度为0
+        if (size==0) return result;
+
+        // 滑动窗口最左边数的index
+        int begin;
+        // 建立一个双端队列
+        ArrayDeque<Integer> q = new ArrayDeque<>();
+        for(int i=0;i<num.length;i++){
+            // begin是窗口起始位置
+            begin = i-size+1;
+            // 队列空，直接加入
+            if(q.isEmpty())
+                q.add(i);
+                // 若队列最左边值已经不在窗口内，直接删除
+            else if(begin > q.peekFirst())
+                q.pollFirst();
+
+            // 从队尾开始比较，把所有比他小的值丢掉
+            while((!q.isEmpty()) && num[q.peekLast()] <= num[i])
+                q.pollLast();
+            // 随后再把它放进去
+            q.add(i);
+
+            // 若窗口起始位置在数组的0位置上或者之后（窗口是完整大小的），才计算窗口的有效最大值
+            if(begin>=0){
+                // 永远是队列最左边最大，加入结果集
+                result.add(num[q.peekFirst()]);
+            }
+        }
+        return result;
+    }
+}
