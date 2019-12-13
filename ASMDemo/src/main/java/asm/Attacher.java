3af4b7c9ee93e9986098fc53f076f8a1b645d41a
yangzhendong01 <yangzhendong01@58.com>
2019-12-13 19:19:33 +0800

    ASMDemo字节码

---
+++ ASMDemo/src/main/java/asm/Attacher.java
@@ -0,0 +1,16 @@
+package asm;
+
+import java.io.IOException;
+
+import com.sun.tools.attach.AgentInitializationException;
+import com.sun.tools.attach.AgentLoadException;
+import com.sun.tools.attach.AttachNotSupportedException;
+import com.sun.tools.attach.VirtualMachine;
+
+public class Attacher {
+    public static void main(String[] args) throws AttachNotSupportedException, IOException, AgentLoadException, AgentInitializationException {
+
+        VirtualMachine vm = VirtualMachine.attach("21124");
+        vm.loadAgent("D:/workspace_java/Java_Practise/ASMDemo/target/ASMDemo-1.0-SNAPSHOT-jar-with-dependencies.jar");
+    }
+}
